db314b19a8e04ebe6047136056008f74552b9e36
Bruno N. Melo <brunonobrega.melo@gmail.com>
2021-01-06 20:27:50 -0300

    ajustando dependências dos factories

--- app/src/main/java/br/com/alura/technews/ui/viewmodels/factory/FormularioNoticiaViewModelFactory.kt
+++ app/src/main/java/br/com/alura/technews/ui/viewmodels/factory/FormularioNoticiaViewModelFactory.kt
@@ -3,14 +3,13 @@ package br.com.alura.technews.ui.viewmodels.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import br.com.alura.technews.repository.noticiaRepository.NoticiaRepository
-import br.com.alura.technews.repository.noticiaRepository.NoticiaRepositoryImpl
 
 class FormularioNoticiaViewModelFactory(private val noticiaRepository: NoticiaRepository) :
     ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass
-            .getConstructor(NoticiaRepositoryImpl::class.java)
+            .getConstructor(NoticiaRepository::class.java)
             .newInstance(noticiaRepository)
     }
 
--- app/src/main/java/br/com/alura/technews/ui/viewmodels/factory/ListaNoticiaViewModelFactory.kt
+++ app/src/main/java/br/com/alura/technews/ui/viewmodels/factory/ListaNoticiaViewModelFactory.kt
@@ -3,14 +3,13 @@ package br.com.alura.technews.ui.viewmodels.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import br.com.alura.technews.repository.noticiaRepository.NoticiaRepository
-import br.com.alura.technews.repository.noticiaRepository.NoticiaRepositoryImpl
 
 class ListaNoticiaViewModelFactory(private val repository: NoticiaRepository) :
     ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass
-            .getConstructor(NoticiaRepositoryImpl::class.java)
+            .getConstructor(NoticiaRepository::class.java)
             .newInstance(repository)
     }
 
